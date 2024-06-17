ca4c4b8dbfe74a6086ed16d2bf77d97a3267f8a1
caggegi <rosariocaggegi99@gmail.com>
2024-06-17 12:28:55 +0200

    reading from controller and filtering by mvset and btns

---
+++ controller.py
@@ -0,0 +1,94 @@
+from inputs import get_gamepad
+import math
+import threading
+
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.DPadX = 0
+        self.DPadY = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+
+    def read(self): # return the buttons/triggers that you care about in this methode
+        pressed = {}
+        pressed['L'] = [self.LeftJoystickY,self.LeftJoystickX]
+        pressed['R'] = [self.RightJoystickY,self.RightJoystickX]
+        pressed['L2'] = self.LeftTrigger
+        pressed['R2'] = self.RightTrigger
+        pressed['L1'] = self.LeftBumper
+        pressed['R1'] = self.RightBumper
+        pressed['A'] = self.A
+        pressed['Y'] = self.X
+        pressed['X'] = self.Y
+        pressed['B'] = self.B
+        pressed['L3'] = self.LeftThumb
+        pressed['R3'] = self.RightThumb
+        pressed['back'] = self.Back
+        pressed['start'] = self.Start
+        pressed['dpad'] = [self.DPadY,self.DPadX]
+        return pressed
+
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.Y = event.state #previously switched with X
+                elif event.code == 'BTN_WEST':
+                    self.X = event.state #previously switched with Y
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'ABS_HAT0X':
+                    self.DPadX = event.state
+                elif event.code == 'ABS_HAT0Y':
+                    self.DPadY = event.state
